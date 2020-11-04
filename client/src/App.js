b389aa0c2ab85095fa5f459f600d682d7e1c07c3
Michael Singh <singh.michaelanthony@gmail.com>
2020-11-03 19:47:58 -0500

    create react hook for handling email input change on sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+// import LandingPage from "./pages/Landing";
 import SignUp from "./pages/SignUp";
 
 import "./App.css";
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+
+  const [email, setEmail] = useState('');
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    console.log('button clicked', event.target);
+  }
+
+  const handleEmailChange = (event) => {
+    console.log(event.target.value);
+    setEmail(event.target.value);
+  }
+
   return (
     <div>
-      <h1>SignUp</h1>
+      <h1>calendapp</h1>
+      <h1>Sign up with Calendapp</h1>
+      <form onSubmit={handleSignUp}>
+        <label>
+          Enter your e-mail to get started
+          <input 
+            value={email}
+            onChange={handleEmailChange}
+            type="email" 
+            name="email" 
+          />
+        </label>
+        <button type="submit">Get started</button>
+      </form>
     </div>
   )
 }
