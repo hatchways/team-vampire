653fca45608d3619dbc4504cb2edbe2d3aa7466c
Michael Singh <singh.michaelanthony@gmail.com>
2020-11-03 20:39:15 -0500

    add links to sign up and login pages if account doesn't exist or does respectively

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
@@ -30,7 +31,7 @@ const Login = () => {
         </label>
         <button type="submit">Continue</button>
       </form>
-      <p>Don't have an account? <a href="#">Sign up</a></p>
+      <p>Don't have an account? <Link to="/">Sign up</Link></p>
     </div>
   )
 }
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
 
@@ -30,6 +31,7 @@ const SignUp = () => {
         </label>
         <button type="submit">Get started</button>
       </form>
+      <p>Already have an account? <Link to="/login"> Log in</Link></p>
     </div>
   )
 }
