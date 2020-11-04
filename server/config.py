a29ad1fc9c6bd2d94fb5909878ce30cb01ff98d2
Omar Quazi <omar@quazi.co>
2020-11-04 14:25:48 -0500

    Changed config.py a little and made app load from config object

--- server/config.py
+++ server/config.py
@@ -1,3 +1,20 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-TEAM_NAME = os.environ['TEAM_NAME']
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    TEAM_NAME = os.getenv('TEAM_NAME')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///example.sqlite"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    pass
+    #SQLALCHEMY_DATABASE_URI = 'mysql://user@localhost/foo'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+
