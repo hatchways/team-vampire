eeeb0af590e5c27b1a21303741ab1f4a7c24b8ad
Omar Quazi <omar@quazi.co>
2020-11-05 10:27:56 -0500

    added first_name, last_name

--- server/models.py
+++ server/models.py
@@ -38,6 +38,8 @@ class Availability(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
     email = db.Column(db.String(80), unique=True)
     timezone = db.Column(db.String(50), nullable=False)
     access_token = db.Column(db.Text, nullable=False)
