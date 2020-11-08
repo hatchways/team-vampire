bbbbea91028169633b3db01f450acc69f1fe4350
Michael Singh <singh.michaelanthony@gmail.com>
2020-11-08 18:54:29 -0500

    merge OAuth2 code from be-signup-oauth2 branch

--- server/app.py
+++ server/app.py
@@ -1,5 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for, redirect, session
 from config import DevelopmentConfig
+import os
+from datetime import timedelta
+
+# dotenv setup
+from dotenv import load_dotenv
+load_dotenv()
 
 # Routes
 from api.ping_handler import ping_handler
@@ -8,6 +14,9 @@ from api.user_handler import user_handler
 from api.meeting_handler import meeting_handler
 from api.appointment_handler import appointment_handler
 from api.availability_handler import availability_handler
+
+#OAuth Library 
+from authlib.integrations.flask_client import OAuth
 
 # Database
 from flask_sqlalchemy import SQLAlchemy
@@ -25,12 +34,28 @@ from models.user import User
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig())
-app.secret_key = "secret key here"
+app.secret_key = os.getenv("APP_SECRET_KEY") # will change to stronger randomly generated password
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
 db.init_app(app)
 migrate = Migrate(app, db, render_as_batch=True, compare_type=True)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+# OAuth Config
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs={'scope': 'openid email profile'},
+)
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
@@ -42,3 +67,57 @@ app.register_blueprint(availability_handler)
 
 if __name__ == '__main__':
     manager.run()
+
+
+# Server Test Route
+@app.route('/', methods=["GET"])
+def hello_world():
+    # email = dict(session).get('profile', {"email": "None"})['email']
+    # print(email)
+    return jsonify(dict(session)), 200
+
+@app.route('/login')
+def login():
+    google = oauth.create_client('google')
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    # print(user_info)
+    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
+    # Here you use the profile/user data that you got and query your database find/register the user
+    # and set ur own data in the session not the profile from google
+    session['profile'] = user_info
+    # Create user in db with info provided
+    # print('user id:')
+    # print(user.sub)
+    # if User.find_by_id(user.sub):
+    #     return {'message': 'User {} already exists'. format(user.name)}
+    # new_user = User(
+    #     id=user.sub, # unique identifier for google user
+    #     username=user.name, 
+    #     email=user.email
+    #     )
+    # print(new_user)
+    # try:
+    #     new_user.save_to_db()
+    #     return {
+    #         'message': 'User {} was created'. format(user.name)
+    #     }
+    # except:
+    #     return {'message': 'Something went wrong'}, 500
+
+    session['token'] = token
+    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
