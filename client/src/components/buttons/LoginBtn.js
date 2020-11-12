2be769de2845e396bf8d6238eb7ad2d0f6195064
Michael Singh <singh.michaelanthony@gmail.com>
2020-11-11 20:10:01 -0500

    implement refresh token to handle new tokenIds to access data and authenticate users

--- client/src/components/buttons/LoginBtn.js
+++ client/src/components/buttons/LoginBtn.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
 
+// refresh token
+import { refreshTokenSetup } from '../utils/refreshTokenSetup';
+
 const clientId = '734201335677-kakgc3np91nr45ss5j9f5l89v6b29h0n.apps.googleusercontent.com';
 
 function LoginBtn() {
     const onSuccess = res => {
         console.log('[Login Success] currentUser:', res.profileObj);
+
+        // initializing the setup
+        refreshTokenSetup(res);
     };
 
     const onFailure = res => {
