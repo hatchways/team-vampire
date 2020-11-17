27f56a6ef03d7815480ebeef3486e99431d65783
Michael Singh <singh.michaelanthony@gmail.com>
2020-11-16 20:44:54 -0500

    remove mongodb dependency and move User schema to ./models/User.js

--- server/app.js
+++ server/app.js
@@ -27,21 +27,6 @@ db.once("open", function(){
 
 // Models
 
-const userSchema = new mongoose.Schema({
-  userName:         { type: String, index: true, unique: true, sparse: true }, // not sure if this is the standard implementation to allow null to not be unique
-  firstName:        String,
-  lastName:         String,
-  email:            { type: String, required: true, unique: true }, // remove unique: true for testing
-  timezone:         String,
-  profilePicture:   String,
-  accessToken:      { type: String, required: true },
-  stripeCustomerId: String,
-  createdAt:        { type: Date, default: Date.now }, 
-  updatedAt:        { type: Date, default: Date.now },
-});
-
-const User = mongoose.model("User", userSchema);
-
 const availabilitySchema = new mongoose.Schema({
   user:       { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   day:        { type: Number, required: true },
---
+++ server/models/User.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+    userName:         { type: String, index: true, unique: true, sparse: true }, // not sure if this is the standard implementation to allow null to not be unique
+    firstName:        String,
+    lastName:         String,
+    email:            { type: String, required: true, unique: true, lowercase: true }, 
+    timezone:         String,
+    profilePicture:   String,
+    accessToken:      { type: String, required: true },
+    stripeCustomerId: String,
+    createdAt:        { type: Date, default: Date.now }, 
+    updatedAt:        { type: Date, default: Date.now },
+  });
+  
+  const User = model("User", userSchema);
+
+  module.exports = { userSchema, User };
