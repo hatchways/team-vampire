19198ec5dd2ec3f142b197f8462fbdb4de2bdd7e
Michael Singh <singh.michaelanthony@gmail.com>
2020-11-19 23:12:29 -0500

    modify user controller routes to use async/await and delete route

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,94 +2,84 @@ const usersRouter = require("express").Router();
 const { User } = require("../models/");
 
 // Create User - WILL REPLACE WITH DATA RETRIEVED FROM GOOGLE AUTHORIZATION
-usersRouter.post("/", (request, response, next) => {
+usersRouter.post("/", async (request, response) => {
     const body = request.body;
+    
     const user = new User({
         userName:    body.userName,
         email:       body.email,
         accessToken: body.accessToken
     });
 
-    user.save()
-        .then(savedUser => {
-            console.log(savedUser);
-            response.json(savedUser);
-        })
-        .catch(error => next(error));
+    const savedUser = await user.save();
+    response.json(savedUser.toJSON());
 });
 
 // Fetch/Read All Users
-usersRouter.get("/", (request, response, next) => {
-    User.find({}).then(users => {
-        response.json(users);
-    })
-        .catch(error => next(error));
+usersRouter.get("/", async (request, response) => {
+    const users = await User.find({});
+    response.json(users.map(user => user.toJSON()));
 });
 
 // Fetch/Read Single User
-usersRouter.get("/:username", (request, response, next) => {
-    const userName = request.params.username;
-    User.findOne({ userName: userName })
-        .then(user => {
-            if (user) {
-                console.log("user", user);
-                response.json(user);
-            } else {
-                response.status(404)
-                    .json({
-                        "status":"error",
-                        "message": "user does not exist"
-                    }).end();
-            }
-        })
-        .catch(error => next(error));
+usersRouter.get("/:username", async (request, response) => {
+    const user = await User.findOne({ userName:request.params.username });
+    if (user) {
+        response.json(user.toJSON());
+    } else {
+        response.status(404)
+            .json({
+                "status":"error",
+                "message": "user does not exist"
+            }).end();
+    }
 });
 
 // Update User - PROBABLY NEEDS TO BE CHANGED TO PATCH ROUTE
 // How to make this an authenticated route?
-usersRouter.patch("/:username", (request, response, next) => {
-    console.log(request.params);
+usersRouter.patch("/:username", async (request, response) => {
     const body = request.body;
 
-    User.findOne({ userName:request.params.username })
-        .then(user => {
-            console.log(user);
-            if (user) { // Update User if the following keys exist in body
-                if (body.userName) {
-                    user.userName = body.userName;
-                }
-                if (body.firstName) {
-                    user.firstName = body.firstName;
-                }
-                if (body.lastName) {
-                    user.lastName = body.lastName;
-                }
-                if (body.email) {
-                    user.email = body.email;
-                }
-                if (body.timezone) {
-                    user.timezone = body.timezone;
-                }
-                if (body.profilePicture) {
-                    user.profilePicture = body.profilePicture;
-                }
+    const user = await User.findOne({ userName:request.params.username });
 
-                user.updatedAt = Date(Date.now());
+    if (user) { // Update User if the following keys exist in body
+        if (body.userName) {
+            user.userName = body.userName;
+        }
+        if (body.firstName) {
+            user.firstName = body.firstName;
+        }
+        if (body.lastName) {
+            user.lastName = body.lastName;
+        }
+        if (body.email) {
+            user.email = body.email;
+        }
+        if (body.timezone) {
+            user.timezone = body.timezone;
+        }
+        if (body.profilePicture) {
+            user.profilePicture = body.profilePicture;
+        }
 
-                user.save();
-                response.json(user);
+        user.updatedAt = Date(Date.now());
+
+        await user.save();
+        response.json(user);
 
 
-            } else {
-                response.status(404)
-                    .json({
-                        "status":"error",
-                        "message": "user does not exist"
-                    }).end();
-            }
+    } else {
+        response.status(404)
+            .json({
+                "status":"error",
+                "message": "user does not exist"
+            }).end();
+    }
+});
 
-        })
-        .catch(error => next(error));
+usersRouter.delete("/:id", async (request, response) => {
+    await User.findByIdAndRemove(request.params.id);
+    response.status(204).end();
 });
 
 module.exports = usersRouter;
